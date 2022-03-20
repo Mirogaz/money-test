7d23cff83ae2a330e815472f76b3ab43c831add2
Damir Akhmadullin <damir@Damirs-MacBook-Air.local>
2022-03-20 21:16:59 +0300

    added footer, HomePage dummy and PageNotFound dummy

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
 import react from 'react'
+import { Footer } from '../Footer/Footer'
 import {Navbar} from '../Navbar/Navbar'
+import { Outlet } from 'react-router'
 
-export const Layout = () => {
+export const Layout = (props) => {
     return (
         <div>
             <Navbar/>
+                <Outlet/>
+            <Footer/>
         </div>
     )
 }
