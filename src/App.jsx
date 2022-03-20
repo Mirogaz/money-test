dc2b46abda1b59b4e02c004d8b0e3dff8499cbc0
Andrey <ver.ver.sus.sus@mail.ru>
2022-03-20 11:49:16 +0300

    test develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './page/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/page/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.scss'
+
+function Header() {
+    return (
+        <div className="menu">
+            <div className="menu__logo">
+                <h1>MONEY</h1>
+                <p>Наш телеграм канал</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Header;
